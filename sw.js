713793b7e95398895bf810af8e00bc9543890611
Enzo Marcolino <jessecontafake@gmail.com>
2024-06-07 17:03:20 -0300

    ababa

--- sw.js
+++ sw.js
@@ -1,36 +1,29 @@
-// Nome do cache (controle de versão)
-const cachePWA = 'cache-v1'
-// Arquivos a serem armazenados em cache
-// Todos os arquivos devem ser adicionados ao vetor(exceto o manifesto)
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/calculo.js',  
-    '/foto.png',
-    '/foto2.png'
-]
+let cacheName = 'cache-v1';
 
-// Instalando o Service Worker e armazenando os arquivos no cache
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cachePWA)
-      .then((cache) => {
-        return cache.addAll(urlsToCache)
-      })
-  )
-})
+self.addEventListener('install', (e) => {
 
-// Interceptando as solicitações de rede e servindo arquivos do cache quando offline
-self.addEventListener('fetch', (event) => {
+  let cache = caches.open(cacheName).then((c) => {
+    c.addAll([
+      // nothing
+    ]);
+  });
+
+  e.waitUntil(cache);
+});
+
+self.addEventListener('fetch', function (event) {
+
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Se o arquivo está no cache, serve o arquivo do cache
-        if (response) {
-          return response
-        }
-        // Caso contrário, faz uma solicitação de rede
-        return fetch(event.request)
-      })
-  )
-})
+
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+
+  );
+
+});
